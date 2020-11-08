be597988c0991277527e8ffc52db076d319f8947
maxshtun999 <maxshtun99@gmail.com>
2020-11-09 00:40:07 +0100

    Selecting group (new view, more easy for understanding)  and updating group works correctly Created Db for Schedule, bug fixes (2 times updated groupname fixed, shutting down server when user enter group fixed, shutting down server when user long time dont change group fixed)

--- handlers/users/settings1.py
+++ handlers/users/settings1.py
@@ -1,6 +1,6 @@
 from aiogram import types
 from aiogram.dispatcher.filters import Command, Text
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message
 from keyboards.default import start, settings1
 from loader import dp
 import psycopg2
@@ -11,8 +11,11 @@ async def show_settings(message: Message):
     await message.answer("Settings", reply_markup=settings1)
 
 
-@dp.message_handler(content_types=["text"])
-async def handle_text(message):
+@dp.message_handler(Text(
+    equals=["P/4/1/I-ENS", "P/4/2/I-ENS", "P/4/3/I-ENS", "P/3/1/I-ENS",
+            "P/3/2/I-ENS", "P/3/3/I-ENS", "P/2/1/I-ENS", "P/2/2/I-ENS",
+            "P/2/3/I-ENS", "P/1/1/I-ENS", "P/1/2/I-ENS", "P/1/3/I-ENS"]))
+async def set_group_name(message: types.Message):
     try:
         connection = psycopg2.connect(user="postgres",
                                       password="1Z2z3z4z",
@@ -20,38 +23,33 @@ async def handle_text(message):
                                       port="5432",
                                       database="Wsei")
         connection.autocommit = True
-        userid = message.from_user.id
+        user_id = message.from_user.id
         text = message.text
         cur = connection.cursor()
+        cur.execute("""DELETE FROM public."Users" WHERE tg_id = (%s)""",
+                    [user_id])
+        print("deleted")
         if text == '':
             print("error empty line")
         else:
-            print(text, userid)
-            cur.execute("""Insert into public."Users" (group_name)  values (%s) """,[text])
-
-            print(text, userid)
+            data = [(user_id, text)]
+            records_list_template = ','.join(['%s'] * len(data))
+            insert_query = """Insert into public."Users" (tg_id, group_name)  values {}""".format(
+                records_list_template)
+            cur.execute(insert_query, data)
             count = cur.rowcount
             print(count, "Record inserted successfully into table")
         connection.commit()
-
 
     except (Exception, psycopg2.Error) as error:
         if (connection):
             print("Failed to insert record into table", error)
 
-
     finally:
         if (connection):
             connection.close()
             print("PostgreSQL connection is closed")
-    await message.answer(f"Your group name is {text}",
-                         reply_markup=ReplyKeyboardRemove())
-
-
-@dp.message_handler(Text(
-    equals=["Change Group name (Send me group name and press this button)"]))
-async def set_group_name(message: types.Message):
-    await message.answer("Go back to Menu", reply_markup=start)
+    await message.answer(f"Your group name is {text}", reply_markup=start)
 
 
 @dp.message_handler(Text(equals=["Go back to Menu"]))
--- keyboards/default/settings1.py
+++ keyboards/default/settings1.py
@@ -3,7 +3,23 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 settings1 = ReplyKeyboardMarkup(
     keyboard=[
         [
-            KeyboardButton(text="Change Group name (Send me group name and press this button)"),
+            KeyboardButton(text="P/4/1/I-ENS"),
+            KeyboardButton(text="P/3/1/I-ENS"),
+            KeyboardButton(text="P/2/1/I-ENS"),
+            KeyboardButton(text="P/1/1/I-ENS"),
+        ],
+        [
+            KeyboardButton(text="P/4/2/I-ENS"),
+            KeyboardButton(text="P/3/2/I-ENS"),
+            KeyboardButton(text="P/2/2/I-ENS"),
+            KeyboardButton(text="P/1/2/I-ENS"),
+
+        ],
+        [
+            KeyboardButton(text="P/4/3/I-ENS"),
+            KeyboardButton(text="P/3/3/I-ENS"),
+            KeyboardButton(text="P/2/3/I-ENS"),
+            KeyboardButton(text="P/1/3/I-ENS"),
         ],
         [
             KeyboardButton(text="Go back to Menu")
